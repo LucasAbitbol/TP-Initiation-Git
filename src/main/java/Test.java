a87eae8d34d3d547392132cfeef21eddfeb10aa1
Gauthier <gauthierpous@bbox.fr>
2020-11-10 15:24:27 +0100

    Commit Lucas

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,3 +1,6 @@
+
+import static java.sql.DriverManager.println;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,4 +13,7 @@
  */
 public class Test {
     //Premiers tests Git
+    public static void main(String[] args) {
+        println("Bien ou quoi ?");
+    }
 }
